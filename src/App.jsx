ebb6ba2a6ee77ecdd74f1515af4f09a575918f74
Lokendrajoci <lokendrajoci@gmail.com>
2025-03-11 14:43:55 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Card from "./Component/Quick/";
+
+function App() {
+  return (
+    <>
+      <div className="bg-white">
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default App;
